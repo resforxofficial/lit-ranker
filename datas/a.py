75922f4326b70e9480aa0b0df6ac7073510776c7
resforxofficial <paedeogim@gmail.com>
2025-01-04 03:04:34 +0900

    commit

--- datas/a.py
+++ datas/a.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from utils import joiner
+from util import joiner
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
